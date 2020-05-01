c21b9937facfaa2f50569792b2efb35990213838
lorenuars19 <lorenuar.s19@gmail.com>
2020-05-01 22:48:18 +0200

    Tried some shit out

---
+++ lib/TowelStyles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class TowelStyles {
+  // Text styles
+  static String _fontFamily = 'Roboto';
+
+  static final titleTextStyle = TextStyle(
+    color: Colors.white,
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 5.0,
+    fontSize: 34,
+  );
+  static final gitTitleTextStyle = TextStyle(
+    color: Colors.black,
+    fontFamily: _fontFamily,
+    letterSpacing: 1.5,
+    fontSize: 28,
+  );
+  static final gitDescTextStyle = TextStyle(
+    color: Colors.black,
+    fontFamily: _fontFamily,
+    letterSpacing: 0,
+    fontSize: 18,
+  );
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,62 @@
 import 'package:flutter/material.dart';
+import 'TowelStyles.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final String _title = 'Towels';
   @override
-  final String _title = 'Towel';
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
-      home: Center(
-      child: Text(_title)
+      home: Towel(title: _title),
+    );
+  }
+}
+
+class Towel extends StatefulWidget {
+  const Towel({
+    Key key,
+    @required String title,
+  })  : _title = title,
+        super(key: key);
+
+  final String _title;
+
+  @override
+  _TowelState createState() => _TowelState();
+}
+
+class _TowelState extends State<Towel> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(widget._title),
+        // TODO implement search bar
+      ),
+      body: Container(
+        child: Card(
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Git TITLE',
+                style: TowelStyles.gitTitleTextStyle,
+              ),
+              Text(
+                'Git Description',
+                style: TowelStyles.gitDescTextStyle,
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        onPressed: () {},
+        tooltip: 'Add new item',
+        child: const Icon(Icons.add),
       ),
     );
   }
