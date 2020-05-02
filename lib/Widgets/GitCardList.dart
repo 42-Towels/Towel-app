062ac1d1a520798f4f22a118fbb4f663ea6bcc71
Artygo8 <you@example.com>
2020-05-02 19:21:19 +0200

    GITICONS

--- lib/Widgets/GitCardList.dart
+++ lib/Widgets/GitCardList.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../Styles/TowelStyles.dart';
+import '../presentation/git_icons_icons.dart';
 
 class GitCardList extends StatelessWidget {
   final entries = {'Git1':'info', 'Git2':'info', 'Git3':'info', 'Git4':'informations', 'Git5':'informations', 'Git6':'informations','Git7':'informations'};
@@ -18,17 +19,23 @@ class GitCardList extends StatelessWidget {
             ),
             margin: EdgeInsetsDirectional.only(top: 8,bottom: 8),
             elevation: 4,
-            child: Column(children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Text('${entries.keys.toList()[index]}', style:TowelStyles.gitTitleTextStyle, ),
-                ]
-              ),
-              Row(
-                children: <Widget>[
-                  Text('${entries[entries.keys.toList()[index]]}', style:TowelStyles.gitDescTextStyle, )
-                ]
-              )
+            child:Row(
+              children:[
+                AspectRatio(aspectRatio: 3/5, child: Icon(GitIcons.github_circled, size: 40,),),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children:[
+                    Padding(
+                      padding: EdgeInsetsDirectional.only(top:8, bottom:4, end:8),
+                      child: Text('${entries.keys.toList()[index]}', style:TowelStyles.gitTitleTextStyle,),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.only(top:4, bottom:4, end:8),
+                      child: Text('${entries[entries.keys.toList()[index]]}', style:TowelStyles.gitDescTextStyle, )
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
@@ -37,3 +44,4 @@ class GitCardList extends StatelessWidget {
     );
   }
 }
+
