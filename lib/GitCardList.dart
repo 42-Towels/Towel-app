e8ed49af19b1e587d5a6ee3f068ec548fc1218fe
Artygo8 <you@example.com>
2020-05-02 16:55:57 +0200

    git card round borders

--- lib/GitCardList.dart
+++ lib/GitCardList.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'TowelStyles.dart';
 
 class GitCardList extends StatelessWidget {
-  final List<String> entries = <String>['Git1', 'Git2', 'Git3'];
+  final entries = {'Git1':'info', 'Git2':'info', 'Git3':'info', 'Git4':'informations', 'Git5':'informations', 'Git6':'informations','Git7':'informations'};
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +11,27 @@ class GitCardList extends StatelessWidget {
       itemCount: entries.length,
       itemBuilder: (BuildContext context, int index) {
         return Container(
-          height: 50,
-          child: Card(child: Text('${entries[index]}', style:TowelStyles.gitTitleTextStyle, )),
+          height: 150,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            margin: EdgeInsetsDirectional.only(top: 8,bottom: 8),
+            elevation: 4,
+            child: Column(children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text('${entries.keys.toList()[index]}', style:TowelStyles.gitTitleTextStyle, ),
+                ]
+              ),
+              Row(
+                children: <Widget>[
+                  Text('${entries[entries.keys.toList()[index]]}', style:TowelStyles.gitDescTextStyle, )
+                ]
+              )
+              ],
+            ),
+          ),
         );
       },
     );
